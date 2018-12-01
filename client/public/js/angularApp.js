68ac5bb3f1895c48c58f8104b1d03fa452abe732
aaron.williams1297@gmail.com <Aaron.williams1297@gmail.com>
2018-12-01 12:15:15 -0500

    Added Facebook Script

--- client/public/js/angularApp.js
+++ client/public/js/angularApp.js
@@ -34,24 +34,24 @@ myFoodBaby.factory('Posts', ['$http', ($http) => {
   /* Different factory methods created by using the $http dependency */
   var o = {
     getData: function () {
-      return $http.get('https://food-baby-web-app.herokuapp.com/api/posts');
-      // return $http.get('http://localhost:4000/api/posts');
+      //return $http.get('https://food-baby-web-app.herokuapp.com/api/posts');
+      return $http.get('http://localhost:4000/api/posts');
     },
     createPost: function (post) {
-      return $http.post('https://food-baby-web-app.herokuapp.com/api/posts', post);
-       // return $http.post('http://localhost:4000/api/posts', post);
+      //return $http.post('https://food-baby-web-app.herokuapp.com/api/posts', post);
+      return $http.post('http://localhost:4000/api/posts', post);
     },
     adminGetData: function () {
-      return $http.get('https://food-baby-web-app.herokuapp.com/api/posts');
-       // return $http.get('http://localhost:4000/admin');
+      //return $http.get('https://food-baby-web-app.herokuapp.com/api/posts');
+      return $http.get('http://localhost:4000/admin');
     },
     deletePost: function (id) {
-      return $http.delete('https://food-baby-web-app.herokuapp.com/api/posts/' + id);
-       // return $http.delete('http://localhost:4000/api/posts/' + id);
+      //return $http.delete('https://food-baby-web-app.herokuapp.com/api/posts/' + id);
+      return $http.delete('http://localhost:4000/api/posts/' + id);
     },
     updateVote: function (id) {
-      return $http.put('https://food-baby-web-app.herokuapp.com/api/posts/' + id);
-       // return $http.put('http://localhost:4000/api/posts/' + id);
+      //return $http.put('https://food-baby-web-app.herokuapp.com/api/posts/' + id);
+      return $http.put('http://localhost:4000/api/posts/' + id);
     }
   };
   return o;
